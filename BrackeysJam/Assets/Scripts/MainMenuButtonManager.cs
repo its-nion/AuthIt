f1cbd042637b41f62b9d149f37c5b6e38f4d8621
Lukas Olesch <lukas.olesch@protonmail.com>
2022-02-26 18:25:39 +0100

    feat: added main menu, music

---
+++ BrackeysJam/Assets/Scripts/MainMenuButtonManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtonManager : MonoBehaviour
+{
+    public Button _musicButton;
+    public Sprite _musicOn;
+    public Sprite _musicOff;
+    
+    public void onStartGamePress()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void onMusicPress()
+    {
+        if (_musicButton.image.sprite == _musicOff)
+        {
+            _musicButton.image.sprite = _musicOn;
+            AudioListener.volume = 0.1f;
+            return;
+        }
+        AudioListener.volume = 0.0f;
+        _musicButton.image.sprite = _musicOff;
+    }
+}
